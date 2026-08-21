1526f029819242fb4e1bf9bc177bcfa24afae395
drmohamedalaa90 <drmohamedalaa90@gmail.com>
2026-08-21 22:34:20 +0300

    Refresh cache for Goals Planner connection

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "medora-";
-const CACHE = "medora-life-mind-safe-v13-plan-timeline";
+const CACHE = "medora-life-mind-safe-v14-goal-plan-bridge";
 
 self.addEventListener("install", () => self.skipWaiting());
 
