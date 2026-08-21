83e58c53bdaddd2f230c0d828d395368345ba10d
drmohamedalaa90 <drmohamedalaa90@gmail.com>
2026-08-22 00:50:20 +0300

    Refresh cache for Activity tracker

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "medora-";
-const CACHE = "medora-life-mind-safe-v20-target-progress";
+const CACHE = "medora-activity-v21";
 
 self.addEventListener("install", () => self.skipWaiting());
 
