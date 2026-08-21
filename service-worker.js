08bd785c159a3aee0ed6ad81e6df58e0be23eccb
drmohamedalaa90 <drmohamedalaa90@gmail.com>
2026-08-22 01:37:36 +0300

    Refresh cache for integrated Progress intelligence

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "medora-";
-const CACHE = "medora-brain-command-center-v29";
+const CACHE = "medora-brain-progress-v30";
 
 self.addEventListener("install", () => self.skipWaiting());
 
