9d66864d453b2bb8690cdd66eedde288396925ae
drmohamedalaa90 <drmohamedalaa90@gmail.com>
2026-08-22 12:36:39 +0300

    Bump cache for Planner edit

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "medora-";
-const CACHE = "medora-soft-keys-continuous-v33";
+const CACHE = "medora-planner-edit-v34";
 
 self.addEventListener("install", () => self.skipWaiting());
 
