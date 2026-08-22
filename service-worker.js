b3e3c4715cead6a54fb13b9c95b29a58e95a3332
drmohamedalaa90 <drmohamedalaa90@gmail.com>
2026-08-22 13:14:40 +0300

    Bump cache for stable plan target progress

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "medora-";
-const CACHE = "medora-plan-links-v39";
+const CACHE = "medora-plan-progress-stable-v40";
 
 self.addEventListener("install", () => self.skipWaiting());
 
