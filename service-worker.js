72fd2fabf0cfb91807d11ea82ca16dbb518fb203
drmohamedalaa90 <drmohamedalaa90@gmail.com>
2026-08-22 17:06:07 +0300

    Finalize Study V2 cache after Travel updates

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "medora-";
-const CACHE = "medora-travel-history-v52";
+const CACHE = "medora-study-v2-travel-history-v53";
 
 self.addEventListener("install", () => self.skipWaiting());
 
