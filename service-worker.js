4218ebec8d9291fd196538515b03df3764dc167b
drmohamedalaa90 <drmohamedalaa90@gmail.com>
2026-08-22 17:58:56 +0300

    Refresh cache for prepared five daily prayers plan

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "medora-";
-const CACHE = "medora-prayer-quality-v1-v47";
+const CACHE = "medora-prepared-prayer-plan-v48";
 
 self.addEventListener("install", () => self.skipWaiting());
 
