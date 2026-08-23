273251316fd7dd776d3b9b941678611af24a409a
drmohamedalaa90 <drmohamedalaa90@gmail.com>
2026-08-23 16:02:39 +0300

    Bump cache for deterministic Study refresh

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "medora-";
-const CACHE = "medora-study-calendar-escape-v59";
+const CACHE = "medora-study-refresh-v60";
 
 self.addEventListener("install", () => self.skipWaiting());
 
