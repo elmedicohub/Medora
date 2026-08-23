aaa4e9f609a8e1ff8ca70185a85a455124797659
drmohamedalaa90 <drmohamedalaa90@gmail.com>
2026-08-23 15:18:21 +0300

    Bump cache for deterministic Study custom date planner

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "medora-";
-const CACHE = "medora-study-custom-date-plans-v54";
+const CACHE = "medora-study-custom-date-plans-v55";
 
 self.addEventListener("install", () => self.skipWaiting());
 
