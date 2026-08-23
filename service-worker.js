8f5b8f6ad97d3510bc4caf28b6e4a8ce6994e26b
drmohamedalaa90 <drmohamedalaa90@gmail.com>
2026-08-23 15:44:31 +0300

    Refresh cache for stable custom Study plan expansion

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "medora-";
-const CACHE = "medora-study-custom-date-plans-v56";
+const CACHE = "medora-study-custom-date-plans-v57";
 
 self.addEventListener("install", () => self.skipWaiting());
 
